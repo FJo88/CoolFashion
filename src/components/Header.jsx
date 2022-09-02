a2ddfc7b8f9dfb7431b36d51da7b031a92952091
AleksandraLos <oluchna@hotmail.com>
2022-09-02 14:52:39 +0200

    add black bg on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -79,9 +79,8 @@ export default function Header() {
     <Popover className="relative bg-black">
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start lg:w-0 lg:flex-1 text-white text-xl">
+          <div className="flex justify-start lg:w-0 lg:flex-1 text-white text-2xl">
             <a href="#">
-              <span className="sr-only">Your Company</span>
               <a
                 className="h-8 w-auto sm:h-10"
               />Cool Fashion
@@ -100,7 +99,7 @@ export default function Header() {
                   <Popover.Button
                     className={classNames(
                       open ? 'text-gray-900' : 'text-gray-500',
-                      'group inline-flex items-center rounded-md bg-black text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                      'group inline-flex items-center  bg-black text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
                     )}
                   >
                     <span>Girls</span>
@@ -123,7 +122,7 @@ export default function Header() {
                     leaveTo="opacity-0 translate-y-1"
                   >
                     <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
-                      <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                      <div className="overflow-hidden rounded-lg shadow-lg ring-1  ring-opacity-5">
                         <div className="relative grid gap-6 bg-black  px-5 py-6 sm:gap-8 sm:p-8">
                           {girls.map((girl) => (
                             <a
@@ -137,14 +136,13 @@ export default function Header() {
                             </a>
                           ))}
                         </div>
-                        <div className="space-y-6 px-5 py-5  sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+                        <div className="space-y-6 px-5 py-5 bg-indigo-500 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
                           {callsToAction.map((item) => (
                             <div key={item.name} className="flow-root">
                               <a
                                 href={item.href}
                                 className="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100"
                               >
-                                <item.icon className="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
                                 <span className="ml-3">{item.name}</span>
                               </a>
                             </div>
@@ -252,7 +250,7 @@ export default function Header() {
                         </div>
                         <div className="bg-black text-gray-50 px-5 py-5 sm:px-8 sm:py-8">
                           <div>
-                            <h3 className="text-base font-medium text-gray-500">Recently Bougt</h3>
+                            <h3 className="text-base font-medium text-indigo-500">Recently Bougt</h3>
                             <ul role="list" className="mt-4 space-y-4">
                               {recentlyBought.map((post) => (
                                 <li key={post.id} className="truncate text-base">
@@ -302,9 +300,9 @@ export default function Header() {
         leaveTo="opacity-0 scale-95"
       >
         <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
-          <div className="divide-y-2 divide-gray-50 rounded-lg bg-black shadow-lg ring-1 ring-black ring-opacity-5">
+          <div className="divide-y-2 divide-gray-50 rounded-lg bg-black shadow-lg ring-1 ring-white ring-opacity-5">
             <div className="px-5 pt-5 pb-6">
-              <div className="flex items-center justify-between">
+              <div className="flex items-center text-white justify-between">
                 <div>
                   <a
                     className="h-8 w-auto"
@@ -323,9 +321,9 @@ export default function Header() {
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
+                      className="-m-3 flex items-center  rounded-md p-3 hover:bg-gray-50"
                     >
-                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                      <span className="ml-3 text-base font-medium text-gray-500">{item.name}</span>
                     </a>
                   ))}
                 </nav>
@@ -333,18 +331,18 @@ export default function Header() {
             </div>
             <div className="space-y-6 py-6 px-5">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-700">
                   Girls
                 </a>
 
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-700">
                   Boys
                 </a>
                 {boys.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                    className="text-base font-medium text-gray-500 hover:text-gray-700"
                   >
                     {item.name}
                   </a>
