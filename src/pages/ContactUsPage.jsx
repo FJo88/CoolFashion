3d689459e54f8a37da9ee6c83a7f0f5f0ecf87a9
Martin Johnsson <maartinjohnsson@gmail.com>
2022-09-06 13:06:26 +0200

    Add routes /admin/new, /category, /contact-us

---
+++ src/pages/ContactUsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactUsPage = () => {
+  return (
+    <div><h1>Hello</h1></div>
+  )
+}
+
+export default ContactUsPage
---
+++ src/pages/FirstPage.jsx
@@ -0,0 +1,22 @@
+import '../App.css';
+import Footer from '../components/Footer';
+import InstagramCarousel from '../components/InstagramCarousel';
+import MainHero from '../components/MainHero';
+import Header from '../components/Header';
+import Main from '../components/Main';
+const FirstPage = () => {
+  return (
+    <>
+    <Header />
+    <MainHero />
+    <Main />
+    <InstagramCarousel />
+    <Footer />
+    </>
+  )
+}
+
+export default FirstPage
+
+
+
