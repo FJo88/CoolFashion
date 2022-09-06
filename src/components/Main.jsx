d1fa501931a49d3e1e4b41b19eed53a56cca8de2
Michiel van der Gragt <michiel@vandergragt.eu>
2022-09-06 15:13:32 +0200

    NEW IN

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ const Main = () => {
     return (  
     <>
       <div className="headers">
-        <h1>Categories</h1>
+        <h1>NEW IN</h1>
       </div>
       <div className="square-container">
         <div className="square top-right">
